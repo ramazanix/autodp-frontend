45958d6aeb1e075183e4cef7034657b80d4d258b
Ramazan <ramazanui2002@gmail.com>
2023-10-14 01:45:51 +0300

    Replace <a> -> <Link>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import AutoDP from "../../public/autodp.svg";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -6,8 +7,8 @@ export default function Header() {
       <header className="container mx-auto text-2xl font-bold max-w-full">
         <nav className="py-1 flex flex-row flex-wrap rounded-b-lg max-h-20 justify-start bg-[#273043]/90 text-[#EFF6EE]">
           <div className="flex flex-row basis-1/4">
-            <a
-              href="#"
+            <Link
+              href="/"
               className="group/logo flex flex-row transition transform hover:-translate-y-0.5 duration-300"
             >
               <div className="bg-clip-text flex bg-gradient-to-r from-red-500 to-amber-500">
@@ -16,29 +17,29 @@ export default function Header() {
                   AutoDP
                 </span>
               </div>
-            </a>
+            </Link>
           </div>
           <div className="flex flex-row basis-2/4 text-xl font-semibold">
             <div className="flex flex-row basis-1/2">
               <div className="m-auto">
-                <a
-                  href="#"
+                <Link
+                  href="/users"
                   className="flex transition transform hover:-translate-y-0.5 duration-300"
                 >
                   <span className="bg-clip-text hover:text-transparent bg-gradient-to-r from-red-500 to-amber-500 duration-300">
                     Users
                   </span>
-                </a>
+                </Link>
               </div>
               <div className="m-auto">
-                <a
-                  href="#"
+                <Link
+                  href="/posts"
                   className="flex transition transform hover:-translate-y-0.5 duration-300"
                 >
                   <span className="bg-clip-text hover:text-transparent bg-gradient-to-r from-red-500 to-amber-500 duration-300">
                     Posts
                   </span>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
