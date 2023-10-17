5f6bff38e592570d9fe31ebc1bcaf97c92ec8b6e
Ramazan <ramazanui2002@gmail.com>
2023-10-17 12:32:50 +0300

    Reformat code by prettier with tailwind plugin

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,29 +4,29 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <>
-      <header className="container mx-auto text-2xl font-bold max-w-full">
-        <nav className="py-1 flex flex-row flex-wrap rounded-b-lg max-h-20 justify-start bg-[#273043]/80 text-[#EFF6EE]">
-          <div className="flex flex-row basis-1/4">
+      <header className="container mx-auto max-w-full text-2xl font-bold">
+        <nav className="flex max-h-20 flex-row flex-wrap justify-start rounded-b-lg bg-[#273043]/80 py-1 text-[#EFF6EE]">
+          <div className="flex basis-1/4 flex-row">
             <Link
               href="/"
-              className="group/logo flex flex-row transition transform hover:-translate-y-0.5 duration-300"
+              className="group/logo flex transform flex-row transition duration-300 hover:-translate-y-0.5"
             >
-              <div className="bg-clip-text flex bg-gradient-to-r from-red-500 to-amber-500">
-                <AutoDP className="fill-[#EFF6EE] group-hover/logo:fill-orange-400 transform transition duration-300" />
-                <span className="m-auto group-hover/logo:text-transparent duration-300">
+              <div className="flex bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text">
+                <AutoDP className="transform fill-[#EFF6EE] transition duration-300 group-hover/logo:fill-orange-400" />
+                <span className="m-auto duration-300 group-hover/logo:text-transparent">
                   AutoDP
                 </span>
               </div>
             </Link>
           </div>
-          <div className="flex flex-row basis-2/4 text-xl font-semibold">
-            <div className="flex flex-row basis-1/2">
+          <div className="flex basis-2/4 flex-row text-xl font-semibold">
+            <div className="flex basis-1/2 flex-row">
               <div className="m-auto">
                 <Link
                   href="/users"
-                  className="flex transition transform hover:-translate-y-0.5 duration-300"
+                  className="flex transform transition duration-300 hover:-translate-y-0.5"
                 >
-                  <span className="bg-clip-text hover:text-transparent bg-gradient-to-r from-red-500 to-amber-500 duration-300">
+                  <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
                     Users
                   </span>
                 </Link>
@@ -34,9 +34,9 @@ export default function Header() {
               <div className="m-auto">
                 <Link
                   href="/posts"
-                  className="flex transition transform hover:-translate-y-0.5 duration-300"
+                  className="flex transform transition duration-300 hover:-translate-y-0.5"
                 >
-                  <span className="bg-clip-text hover:text-transparent bg-gradient-to-r from-red-500 to-amber-500 duration-300">
+                  <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
                     Posts
                   </span>
                 </Link>
