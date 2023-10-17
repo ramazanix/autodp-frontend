b3fcdd8aa1815488df9c603fa12e349b04fb7b12
Ramazan <ramazanui2002@gmail.com>
2023-10-17 22:09:23 +0300

    Create customButon component

---
+++ src/components/customButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+enum BackgroundColorsStyles {
+  red = 'bg-red-300/70 hover:bg-red-300 focus:ring-red-300',
+  yellow = 'bg-yellow-300/70 hover:bg-yellow-300 focus:ring-yellow-300',
+  green = 'bg-green-300/70 hover:bg-green-300 focus:ring-green-300',
+  blue = 'bg-blue-300/70 hover:bg-blue-300 focus:ring-blue-300',
+}
+
+interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+  text: string
+  type_: 'submit' | 'button' | 'reset'
+  onClick: React.MouseEventHandler
+  bgColor?: keyof typeof BackgroundColorsStyles
+}
+
+export const CustomButton: React.FC<ButtonProps> = ({
+  text,
+  bgColor = 'blue',
+  type_,
+  onClick,
+}) => {
+  return (
+    <>
+      <button
+        type={type_}
+        className={`w-full rounded-lg px-5 py-2.5 text-sm font-medium text-gray-900/70 duration-200 focus:outline-none focus:ring-2 ${BackgroundColorsStyles[bgColor]}`}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </>
+  )
+}
