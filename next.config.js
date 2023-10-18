5af3af62ed4a2b3630876d87871b63531fcf53f6
Ramazan <ramazanui2002@gmail.com>
2023-10-18 21:57:08 +0300

    Add rewrites

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-        });
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
 
-        return config;
-    }
+    return config
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
