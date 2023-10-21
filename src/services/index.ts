e60c1f973dcead29565634745eb8ba03d48c3956
Ramazan <ramazanui2002@gmail.com>
2023-10-21 15:06:45 +0300

    Change url for authService instance

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,3 @@
 import { AuthService } from '@/services/authService'
 
-export const authService = new AuthService('//localhost:80')
+export const authService = new AuthService('//localhost:80/auth')
