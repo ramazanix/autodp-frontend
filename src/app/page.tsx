d68270e6f9a4ef6eb924d0a0b9698e0bafea5020
Ramazan <ramazanui2002@gmail.com>
2023-10-21 22:04:28 +0300

    Added interaction with currentUser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import Header from '@/components/header'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+  const { user } = useCurrentUser(router)
   return (
     <>
       <Header />
+      {user ? user.username : 'Not Authorized'}
     </>
   )
 }
