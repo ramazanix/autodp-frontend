1e00bc32dde3e6395f542a83a3d94bb3d9236500
Ramazan <ramazanui2002@gmail.com>
2023-10-21 15:11:06 +0300

    Delete unused import

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
 import { AuthTokens, IUser } from '@/app/types'
 import { authService } from '@/services'
