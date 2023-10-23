ae8f5985ee2edbdd3151cff09bdb7e1ea5bed3c2
Ramazan <ramazanui2002@gmail.com>
2023-10-24 00:12:31 +0300

    Added similarity of logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@
 import Header from '@/components/header'
 import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { useRouter } from 'next/navigation'
+import { useLogout } from '@/hooks/useLogout'
 
 export default function Home() {
   const router = useRouter()
   const { user } = useCurrentUser(router)
+  const { logout } = useLogout()
   return (
     <>
       <Header />
       {user ? user.username : 'Not Authorized'}
+      {user && <button onClick={logout}>Log Out</button>}
     </>
   )
 }
