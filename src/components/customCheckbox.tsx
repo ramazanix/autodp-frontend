a2836d6089ade95081d0ab88d56a73e7928a1517
Ramazan <ramazanui2002@gmail.com>
2023-10-23 23:48:34 +0300

    Add onClick prop

--- src/components/customCheckbox.tsx
+++ src/components/customCheckbox.tsx
@@ -11,6 +11,7 @@ interface Props {
   id: string
   ariaDescribedby?: string
   text: string
+  onClick: React.MouseEventHandler
   bgColor?: keyof typeof BackgroundColorsStyles
 }
 
@@ -18,6 +19,7 @@ export const CustomCheckbox: React.FC<Props> = ({
   id,
   ariaDescribedby,
   text,
+  onClick,
   bgColor = 'red',
 }) => {
   return (
@@ -27,12 +29,13 @@ export const CustomCheckbox: React.FC<Props> = ({
           <input
             type="checkbox"
             id={id}
+            onClick={onClick}
             aria-describedby={ariaDescribedby ? ariaDescribedby : id}
             className={`h-4 w-4 cursor-pointer rounded border border-gray-500 duration-200  focus:ring-0 focus:ring-offset-0 ${BackgroundColorsStyles[bgColor]}`}
           />
         </div>
         <div className="-mb-1 ml-2 inline-flex align-top text-sm">
-          <label htmlFor="remember" className="text-gray-500">
+          <label htmlFor={id} className="text-gray-500">
             {text}
           </label>
         </div>
