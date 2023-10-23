d829ce4dae0b55ec658fd7c5697827243bb3348c
Ramazan <ramazanui2002@gmail.com>
2023-10-23 23:53:57 +0300

    Add revokening tokens and removing it from cookies

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,9 +1,16 @@
 import Cookies from 'js-cookie'
+import { authService } from '@/services'
 
 export const useLogout = () => {
-  const logout = () => {
-    Cookies.remove('accessToken')
-    Cookies.remove('refreshToken')
+  const logout = async () => {
+    const accessToken = Cookies.get('accessToken')
+    const refreshToken = Cookies.get('refreshToken')
+    if (accessToken && refreshToken) {
+      await authService.revokeAccessToken(accessToken)
+      await authService.revokeRefreshToken(refreshToken)
+      Cookies.remove('accessToken')
+      Cookies.remove('refreshToken')
+    }
   }
 
   return { logout }
