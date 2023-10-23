e774ea06ee48f0c40cf6862e6c6c00a798f82933
Ramazan <ramazanui2002@gmail.com>
2023-10-24 00:13:01 +0300

    Added similarity of fetching users list

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,32 @@
+'use client'
+
 import Header from '@/components/header'
+import { useState, useEffect } from 'react'
+import { IUser } from '@/app/types'
 
 export default function UsersPage() {
-  return <Header />
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch('/api/users')
+      const data = await response.json()
+      setUsers(data)
+    }
+    fetchUsers().catch(console.error)
+  }, [])
+  return (
+    <>
+      <Header />
+      <div>
+        {users.map((user, idx) => {
+          return (
+            <p key={user.id}>
+              {idx + 1} {user.username} {user.role.name} {user.created_at}
+            </p>
+          )
+        })}
+      </div>
+    </>
+  )
 }
