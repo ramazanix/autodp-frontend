f20f5caee3034aea26ea2c71a3a60634948ac8dd
Ramazan <ramazanui2002@gmail.com>
2023-10-24 23:49:44 +0300

    Define userService instance

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
 import { AuthService } from '@/services/authService'
+import { UserService } from '@/services/userService'
 
 export const authService = new AuthService('//localhost:80/auth')
+export const usersService = new UserService('//localhost:80/users')
