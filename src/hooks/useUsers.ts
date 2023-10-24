6deaf4e5bdad61b057e0c0b1ac89bcb24650b5c6
Ramazan <ramazanui2002@gmail.com>
2023-10-24 23:53:27 +0300

    Create hook for getting usersList

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,20 @@
+import { usersService } from '@/services'
+import { IUser } from '@/app/types'
+import { useEffect, useState } from 'react'
+
+interface Props {
+  limit?: number
+}
+
+export const useUsers = (props: Props) => {
+  const [usersList, setUsersList] = useState<IUser[] | []>([])
+  const { limit } = props
+  useEffect(() => {
+    usersService
+      .users(limit)
+      .then((users) => setUsersList(users))
+      .catch((e) => console.log(e))
+  }, [limit])
+
+  return { usersList }
+}
