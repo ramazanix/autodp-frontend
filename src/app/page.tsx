51366abe972812e924d8e806ffb7f7c86b6faffc
Ramazan <ramazanui2002@gmail.com>
2023-10-24 14:42:17 +0300

    Add logout button redirect after clicking it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,23 @@
 
 import Header from '@/components/header'
 import { useCurrentUser } from '@/hooks/useCurrentUser'
-import { useRouter } from 'next/navigation'
 import { useLogout } from '@/hooks/useLogout'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
-  const { user } = useCurrentUser(router)
+  const { user } = useCurrentUser()
   const { logout } = useLogout()
+
+  const handleLogout = () => {
+    logout().then(() => router.push('/login'))
+  }
+
   return (
     <>
       <Header />
       {user ? user.username : 'Not Authorized'}
-      {user && <button onClick={logout}>Log Out</button>}
+      {user && <button onClick={handleLogout}>Log Out</button>}
     </>
   )
 }
