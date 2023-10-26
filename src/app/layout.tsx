4f5b636636908cf9f5271cce17809ef65eeb013a
Ramazan <ramazanui2002@gmail.com>
2023-10-26 23:56:57 +0300

    Add Providers component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Providers } from '@/providers'
 
 export const metadata: Metadata = {
   title: 'AutoDP',
@@ -14,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex min-h-screen w-full flex-col bg-gradient-to-b from-blue-300 to-red-500/60">
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
