e1e859cba1480168eea97fafdfc7f7ad605980bb
Ramazan <ramazanui2002@gmail.com>
2023-10-28 01:31:07 +0300

    Added user to props in Head component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
 'use client'
 
-import Header from '@/components/header'
+import { Header } from '@/components/header'
 import { useCurrentUser } from '@/hooks/useCurrentUser'
-import { useLogout } from '@/hooks/useLogout'
-import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
   const { user } = useCurrentUser()
-  const { logout } = useLogout()
-
-  const handleLogout = () => {
-    logout().then(() => router.push('/login'))
-  }
 
   return (
     <>
-      <Header />
+      <Header user={user}/>
       {user ? user.username : 'Not Authorized'}
-      {user && <button onClick={handleLogout}>Log Out</button>}
     </>
   )
 }
