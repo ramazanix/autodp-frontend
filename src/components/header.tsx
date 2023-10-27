3ffb199516e8d3d75750cd58b7c3ed4424a1bc7b
Ramazan <ramazanui2002@gmail.com>
2023-10-28 01:29:51 +0300

    Added links that depends on user state

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,26 @@
 import AutoDP from '../../public/autodp.svg'
 import Link from 'next/link'
+import { IUser } from '@/app/types'
+import React from 'react'
+import { useLogout } from '@/hooks/useLogout'
+import { useRouter } from 'next/navigation'
 
-export default function Header() {
+interface Props {
+  user: IUser | null
+}
+
+export const Header: React.FC<Props> = ({ user }) => {
+  const router = useRouter()
+  const { logout } = useLogout()
+
+  const handleLogout = () => {
+    logout().then(() => router.push('/login'))
+  }
+
   return (
     <>
       <header className="container mx-auto max-w-full text-2xl font-bold">
-        <nav className="flex max-h-20 flex-row flex-wrap justify-start rounded-b-lg bg-[#273043]/80 py-1 text-[#EFF6EE]">
+        <nav className="flex max-h-20 flex-row flex-nowrap justify-start rounded-b-lg bg-[#273043]/80 py-1 text-[#EFF6EE]">
           <div className="flex basis-1/4 flex-row">
             <Link
               href="/"
@@ -43,6 +58,51 @@ export default function Header() {
               </div>
             </div>
           </div>
+          {user ? (
+            <div className="flex basis-1/4 flex-row text-xl font-semibold">
+              <div className="m-auto ml-24 flex basis-1/2 justify-end">
+                <Link
+                  href={`/users/${user?.username}`}
+                  className="flex transform transition duration-300 hover:-translate-y-0.5"
+                >
+                  <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
+                    My Profile
+                  </span>
+                </Link>
+              </div>
+              <div className="m-auto -ml-10 flex basis-1/2 justify-end pr-5">
+                <button
+                  className="transform bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text transition duration-300 hover:-translate-y-0.5 hover:text-transparent"
+                  onClick={handleLogout}
+                >
+                  <span>Log Out</span>
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex basis-1/4 flex-row text-xl font-semibold">
+              <div className="m-auto ml-24 flex basis-1/2 justify-end">
+                <Link
+                  href="/login"
+                  className="flex transform transition duration-300 hover:-translate-y-0.5"
+                >
+                  <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
+                    Sign In
+                  </span>
+                </Link>
+              </div>
+              <div className="m-auto -ml-10 flex basis-1/2 justify-end pr-5">
+                <Link
+                  href="/register"
+                  className="flex transform transition duration-300 hover:-translate-y-0.5"
+                >
+                  <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
+                    Sign Up
+                  </span>
+                </Link>
+              </div>
+            </div>
+          )}
         </nav>
       </header>
     </>
