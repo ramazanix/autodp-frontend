0d52161c4f651fa912c06468cd584f2fe3e83ccb
Ramazan <ramazanui2002@gmail.com>
2023-10-28 16:55:09 +0300

    Change <a> to <Link> and create parent div

--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -1,5 +1,6 @@
 import { IUser } from '@/app/types'
 import React from 'react'
+import Link from 'next/link'
 
 interface Props {
   user: IUser
@@ -8,14 +9,16 @@ interface Props {
 
 export const UserCard: React.FC<Props> = ({ user }) => {
   return (
-    <a
-      href={`/users/${user.username}`}
-      className="block max-w-sm rounded-lg border border-black bg-[#273043]/50 p-6 shadow duration-300 hover:bg-[#273043]/60"
-    >
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-        {user.username}
-      </h5>
-      <p className="font-extralight text-white">{user.role.description}</p>
-    </a>
+    <div className="mx-10">
+      <Link
+        href={`/users/${user.username}`}
+        className="block max-w-sm rounded-lg border border-black bg-[#273043]/50 p-6 shadow duration-300 hover:bg-[#273043]/60"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+          {user.username}
+        </h5>
+        <p className="font-extralight text-white">{user.role.description}</p>
+      </Link>
+    </div>
   )
 }
