25d3109b7cccd7f0e85b4c44321d3099eb5d52a8
Ramazan <ramazanui2002@gmail.com>
2023-10-30 22:21:58 +0300

    Create function to parse field errors and use it in setErrors

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
+import { FieldError } from '@/app/types'
+
 export function Capitalize(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1)
+}
+
+export function ParseFieldErrors(data: Array<FieldError>) {
+  let result: { [key: string]: string } = {}
+
+  data.map((field) => {
+    let fieldName = field.loc.at(-1)
+    result[fieldName!] = Capitalize(field.msg)
+  })
+
+  return result
 }
