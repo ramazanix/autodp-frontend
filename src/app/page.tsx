0e210f40d65e7b0a8ef855eacc30556b54479a9e
Ramazan <ramazanui2002@gmail.com>
2023-11-03 23:07:46 +0300

    Added resetUser usage and rehydration state data from localStorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 'use client'
 
 import { Header } from '@/components/header'
-import { useCurrentUser } from '@/hooks/useCurrentUser'
+import useUserStore from '@/store/store'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const { user, userIsLoading } = useCurrentUser()
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    useUserStore.persist.rehydrate()
+    setIsLoading(false)
+  }, [])
+
+  const user = useUserStore((state) => state.user)
   return (
     <>
-      <Header user={user} userIsLoading={userIsLoading} />
-      {userIsLoading ? '' : user ? user.username : 'Not Authorized'}
+      <Header user={user} userIsLoading={isLoading} />
+      {user ? user.username : 'Not Authorized'}
     </>
   )
 }
