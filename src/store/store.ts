f9ac96a9bb831ebd767f5558e187d90d1dc5cb52
Ramazan <ramazanui2002@gmail.com>
2023-11-03 23:04:49 +0300

    Create store

---
+++ src/store/store.ts
@@ -0,0 +1,25 @@
+import { create } from 'zustand'
+import { IUser } from '@/app/types'
+import { persist } from 'zustand/middleware'
+
+interface UserState {
+  user: IUser | null
+  setUser: (userData: IUser) => void
+  resetUser: () => void
+}
+
+const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (newData) =>
+        set((state) => ({
+          user: newData,
+        })),
+      resetUser: () => set((state) => ({ user: null })),
+    }),
+    { name: 'user-storage', skipHydration: true }
+  )
+)
+
+export default useUserStore
