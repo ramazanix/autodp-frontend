79d2c5a2315db9993c93499e1c4b61f48fc25e66
Ramazan <ramazanui2002@gmail.com>
2023-11-10 23:53:18 +0300

    Create func to parse dates

--- src/utils.ts
+++ src/utils.ts
@@ -14,3 +14,16 @@ export function ParseFieldErrors(data: Array<FieldError>) {
 
   return result
 }
+
+export function parseDate(stringDate: string): Date {
+  let arr = stringDate.split(' ')
+  let date = arr[1].split('.')
+
+  let temp = date[0]
+  date[0] = date[1]
+  date[1] = temp
+
+  arr[1] = date.join('.')
+
+  return new Date(arr.join(' '))
+}
