12d23441f2d3b102419331328cabe4b6f62efd9c
Ramazan <ramazanui2002@gmail.com>
2023-11-11 23:36:06 +0300

    Use usersService via UsersClient on fetch instead of UserService on axios

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import { AuthService } from '@/services/authService'
-import { UserService } from '@/services/userService'
+import UsersClient from '@/services/UsersClient'
 
 export const authService = new AuthService('//localhost:80/auth')
-export const usersService = new UserService('//localhost:80/users')
+export const usersService = new UsersClient('http://localhost:80/users')
