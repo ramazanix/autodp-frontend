2955d0695120b46dc0d83aab4e64e06e585ef853
Ramazan <ramazanui2002@gmail.com>
2023-11-11 22:44:07 +0300

    Created UsersClient

---
+++ src/services/UsersClient.ts
@@ -0,0 +1,17 @@
+import HttpClient from '@/services/HttpClient'
+
+class UsersClient extends HttpClient {
+  constructor(baseURL: string) {
+    super({
+      baseURL,
+      headers: new Headers(),
+    })
+  }
+
+  users = {
+    getAll: (limit?: number) => this.get(`?limit=${limit}`, {parseResponse: true}),
+    get: (username: string) => this.get(`/${username}`, {parseResponse: true}),
+  }
+}
+
+export default UsersClient
