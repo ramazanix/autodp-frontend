8e01f9159a8ea2093e16709312b818dac1c6e6f1
Ramazan <ramazanui2002@gmail.com>
2023-11-11 23:45:44 +0300

    Use fetch client instead of axios

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,14 +1,20 @@
 import { usersService } from '@/services'
 import { UserProfile } from '@/components/userProfile'
 import { PrivateWarning } from '@/components/privateWarning'
+import { cookies } from 'next/headers'
 
 export default async function UserPage({
   params,
 }: {
   params: { username: string }
 }) {
-  const userInfo = await usersService.user(params.username)
-  return (
-    <>{userInfo ? <UserProfile userInfo={userInfo!} /> : <PrivateWarning />}</>
-  )
+  const accessToken = cookies().get('accessToken')?.value
+
+  if (accessToken) {
+    const userInfo = await usersService
+      .setBearerAuth(accessToken)
+      .users.get(params.username)
+
+    return <>{<UserProfile userInfo={userInfo!} />}</>
+  } else return <PrivateWarning />
 }
