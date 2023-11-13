900886e82d095cdf8a77284cf3c52ad1cf456fa7
Ramazan <ramazanui2002@gmail.com>
2023-11-13 21:30:18 +0300

    Remove unused import ad replace router.refresh()

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import { CustomCheckbox } from '@/components/customCheckbox'
 import { CustomButton } from '@/components/customButton'
 import { useLogin } from '@/hooks/useLogin'
 import React, { useState } from 'react'
-import { redirect, useRouter, useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { ParseFieldErrors } from '@/utils'
 
 export default function LoginPage() {
@@ -66,10 +66,10 @@ export default function LoginPage() {
       if (authTokens.status === 'success') {
         if (backRef) {
           router.replace(backRef)
-          router.refresh()
         } else {
           router.replace('/')
         }
+        router.refresh()
       } else {
         if (authTokens.statusCode === 401) {
           setErrors({
