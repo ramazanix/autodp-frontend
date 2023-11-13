8490aed3901705744a936a3d588b291b85a1be88
Ramazan <ramazanui2002@gmail.com>
2023-11-13 21:54:01 +0300

    Add types and transform strings into date objects

--- src/services/UsersClient.ts
+++ src/services/UsersClient.ts
@@ -1,4 +1,6 @@
 import HttpClient from '@/services/HttpClient'
+import { IUser } from '@/app/types'
+import { parseDate } from '@/utils'
 
 class UsersClient extends HttpClient {
   constructor(baseURL: string) {
@@ -9,10 +11,20 @@ class UsersClient extends HttpClient {
   }
 
   users = {
-    getAll: (limit?: number) =>
+    getAll: (limit?: number): Promise<IUser[]> =>
       this.get(`?limit=${limit}`, { parseResponse: true }),
-    get: (username: string) =>
-      this.get(`/${username}`, { parseResponse: true }),
+
+    get: (username: string): Promise<IUser> =>
+      this.get(`/${username}`, { parseResponse: true }).then((res) => {
+        let created_at = parseDate(res.created_at)
+        let updated_at = parseDate(res.updated_at)
+
+        return {
+          ...res,
+          created_at,
+          updated_at,
+        }
+      }),
   }
 }
 
