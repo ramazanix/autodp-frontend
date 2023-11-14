d5c97cd41bff61552f3aaa7af875d8c3bc13817f
Ramazan <ramazanui2002@gmail.com>
2023-11-14 23:21:42 +0300

    Refactor to server component

--- src/app/(main)/users/page.tsx
+++ src/app/(main)/users/page.tsx
@@ -1,29 +1,7 @@
-'use client'
+import { UsersFinder } from '@/components/usersFinder'
+import { usersService } from '@/services'
 
-import { useUsers } from '@/hooks/useUsers'
-import { useState } from 'react'
-import { UsersList } from '@/components/usersList'
-import { SearchBar } from '@/components/searchBar'
-
-export default function UsersPage() {
-  const limit = 10
-  const { usersList } = useUsers({ limit })
-  const [inputText, setInputText] = useState('')
-  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let lowerCase = e.target.value.toLowerCase()
-    setInputText(lowerCase)
-  }
-
-  return (
-    <>
-      <SearchBar
-        id={'users-filter'}
-        name={'users-filter'}
-        placeholder={'Find user'}
-        value={inputText}
-        onChange={inputHandler}
-      />
-      <UsersList users={usersList} input={inputText} />
-    </>
-  )
+export default async function UsersPage() {
+  const res = await usersService.users.getAll()
+  return <UsersFinder usersList={res.data} />
 }
