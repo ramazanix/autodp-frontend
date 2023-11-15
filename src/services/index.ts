cc614006a2db4464273e6fcb85c6d3b55826a2d2
Ramazan <ramazanui2002@gmail.com>
2023-11-15 20:47:01 +0300

    Use new AuthClient

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
-import { AuthService } from '@/services/authService'
 import UsersClient from '@/services/UsersClient'
+import AuthClient from '@/services/AuthClient'
 
-export const authService = new AuthService('//localhost:80/auth')
+export const authService = new AuthClient('http://localhost:80/auth')
 export const usersService = new UsersClient('http://localhost:80/users')
