f72f99cebc7b77d63dad77ced2c7e54786857925
Ramazan <ramazanui2002@gmail.com>
2023-11-24 22:02:56 +0300

    Added active and disabled states for route links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { IUser } from '@/app/types'
 import React from 'react'
 import { useLogout } from '@/hooks/useLogout'
 import { useRouter } from 'next/navigation'
-
+import { usePathname } from 'next/navigation'
 interface Props {
   user: IUser | null
   userIsLoading: boolean
@@ -13,6 +13,21 @@ interface Props {
 export const Header: React.FC<Props> = ({ user, userIsLoading }) => {
   const router = useRouter()
   const { logout } = useLogout()
+  const pathname = usePathname()
+  const linkBaseClass = 'flex transform transition duration-300 '
+  const linkActive = 'hover:-translate-y-0.5'
+  const linkDisabled = 'cursor-default pointer-events:none'
+  const linkStatuses = {
+    home:
+      'group/logo ' +
+      linkBaseClass +
+      (pathname !== '/' ? linkActive : linkDisabled),
+    users: linkBaseClass + (pathname !== '/users' ? linkActive : linkDisabled),
+    posts: linkBaseClass + (pathname !== '/posts' ? linkActive : linkDisabled),
+    profile:
+      linkBaseClass +
+      (pathname !== `/users/${user?.username}` ? linkActive : linkDisabled),
+  }
 
   const handleLogout = () => {
     logout().then(() => router.push('/login'))
@@ -23,10 +38,7 @@ export const Header: React.FC<Props> = ({ user, userIsLoading }) => {
       <header className="container mx-auto max-w-full text-2xl font-bold">
         <nav className="flex max-h-20 flex-row flex-nowrap justify-start rounded-b-lg bg-[#273043]/80 py-1 text-[#EFF6EE]">
           <div className="flex basis-1/4 flex-row">
-            <Link
-              href="/"
-              className="group/logo flex transform flex-row transition duration-300 hover:-translate-y-0.5"
-            >
+            <Link href="/" className={linkStatuses.home}>
               <div className="flex bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text">
                 <AutoDP className="transform fill-[#EFF6EE] transition duration-300 group-hover/logo:fill-orange-400" />
                 <span className="m-auto duration-300 group-hover/logo:text-transparent">
@@ -38,20 +50,14 @@ export const Header: React.FC<Props> = ({ user, userIsLoading }) => {
           <div className="flex basis-2/4 flex-row text-xl font-semibold">
             <div className="flex basis-1/2 flex-row">
               <div className="m-auto">
-                <Link
-                  href={'/users'}
-                  className="flex transform transition duration-300 hover:-translate-y-0.5"
-                >
+                <Link href={'/users'} className={linkStatuses.users}>
                   <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
                     Users
                   </span>
                 </Link>
               </div>
               <div className="m-auto">
-                <Link
-                  href={'/posts'}
-                  className="flex transform transition duration-300 hover:-translate-y-0.5"
-                >
+                <Link href={'/posts'} className={linkStatuses.posts}>
                   <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
                     Posts
                   </span>
@@ -66,7 +72,7 @@ export const Header: React.FC<Props> = ({ user, userIsLoading }) => {
               <div className="m-auto ml-24 flex basis-1/2 justify-end">
                 <Link
                   href={`/users/${user?.username}`}
-                  className="flex transform transition duration-300 hover:-translate-y-0.5"
+                  className={linkStatuses.profile}
                 >
                   <span className="bg-gradient-to-r from-red-500 to-amber-500 bg-clip-text duration-300 hover:text-transparent">
                     My Profile
