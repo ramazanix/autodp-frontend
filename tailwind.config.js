d80734286712229f04320ba8e2bc1dd93af3f986
Ramazan <ramazanui2002@gmail.com>
2023-11-24 14:59:35 +0300

    Reformat code with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('autoprefixer'),
-  ],
+  plugins: [require('@tailwindcss/forms'), require('autoprefixer')],
 }
