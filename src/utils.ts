98a84c869b7fda19d41d6530094847c9da920527
Ramazan <ramazanui2002@gmail.com>
2023-11-26 17:49:21 +0300

    Created function for converting dataURL to File object

--- src/utils.ts
+++ src/utils.ts
@@ -27,3 +27,17 @@ export function parseDate(stringDate: string): Date {
 
   return new Date(arr.join(' '))
 }
+
+export function dataURLtoFile(dataurl: string, filename: string) {
+  let arr = dataurl.split(',')
+  let mime = arr[0].match(/:(.*?);/)![1]
+  let bstr = atob(arr[1])
+  let n = bstr.length
+  let u8arr = new Uint8Array(n)
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+
+  return new File([u8arr], filename, { type: mime })
+}
