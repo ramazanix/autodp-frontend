df7fd27e5b461eb10ed2a6a81658be5fc82cd779
Ramazan <ramazanui2002@gmail.com>
2023-11-27 11:19:51 +0300

    Using .env variable

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,9 @@
 import UsersClient from '@/services/UsersClient'
 import AuthClient from '@/services/AuthClient'
 
-export const authService = new AuthClient('http://localhost:80/auth')
-export const usersService = new UsersClient('http://localhost:80/users')
+export const authService = new AuthClient(
+  `${process.env.NEXT_PUBLIC_API_URL}/auth`
+)
+export const usersService = new UsersClient(
+  `${process.env.NEXT_PUBLIC_API_URL}/users`
+)
