7f5509dcd55d46e26f1b6c627612b3c60f6e26b9
Ramazan <ramazanui2002@gmail.com>
2023-11-27 11:18:32 +0300

    Added loading state depends on user avatar

--- src/components/userProfile.tsx
+++ src/components/userProfile.tsx
@@ -1,21 +1,35 @@
+'use client'
+
 import { IUser } from '@/app/types'
 import Image from 'next/image'
+import { useState } from 'react'
 
 interface Prop {
   userInfo: IUser
 }
 
 export const UserProfile: React.FC<Prop> = ({ userInfo }) => {
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
-    <div className="mx-auto my-6 grid w-1/2 grid-cols-2 grid-rows-4 rounded-2xl bg-[#273043]/50 shadow">
+    <div
+      className={
+        isLoading
+          ? 'hidden'
+          : 'mx-auto my-6 grid w-1/2 grid-cols-2 grid-rows-4 rounded-2xl bg-[#273043]/50 shadow'
+      }
+    >
       <Image
         className="row-span-4 h-full w-full rounded-full p-10"
-        src={'http://localhost' + userInfo.avatar.location}
+        src={process.env.NEXT_PUBLIC_API_URL + userInfo.avatar.location}
         alt="User avatar"
         width={0}
         height={0}
         sizes="100vw"
         priority
+        onLoad={() => {
+          setIsLoading(!isLoading)
+        }}
       />
 
       <div className="row-span-2 flex items-center justify-center text-5xl">
