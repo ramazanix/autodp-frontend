6f1b72aa6005fcdf90fc3ebfebc1d58ebb9db4ba
Ramazan <ramazanui2002@gmail.com>
2023-11-29 23:54:44 +0300

    Provide accessToken to UserProfile

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -19,6 +19,7 @@ export default async function UserPage({
       <UserProfile
         userInfo={userInfo}
         owner={params.username === parsedJWT.sub}
+        accessToken={accessToken}
       />
     )
   }
