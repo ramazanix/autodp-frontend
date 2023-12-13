bd8b1a20a52275c9e905b525849f9bc8f7949a33
Ramazan <ramazanui2002@gmail.com>
2023-12-13 21:43:53 +0300

    Removed calculating owner by decoding jwt token

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -2,7 +2,6 @@ import { usersService } from '@/services'
 import { UserProfile } from '@/components/userProfile'
 import { PrivateWarning } from '@/components/privateWarning'
 import { cookies } from 'next/headers'
-import { jwtDecode } from 'jwt-decode'
 
 export default async function UserPage({
   params,
@@ -13,15 +12,8 @@ export default async function UserPage({
 
   if (accessToken) {
     const userInfo = await usersService.users.get(params.username, accessToken)
-    const parsedJWT = jwtDecode(accessToken)
 
-    return (
-      <UserProfile
-        userInfo={userInfo}
-        owner={params.username === parsedJWT.sub}
-        accessToken={accessToken}
-      />
-    )
+    return <UserProfile userInfo={userInfo} accessToken={accessToken} />
   }
 
   return <PrivateWarning />
