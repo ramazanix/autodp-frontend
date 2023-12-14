85d05ee74ae5d4f98c99704a6a186c74c73d0d5f
Ramazan <ramazanui2002@gmail.com>
2023-12-15 00:49:28 +0300

    Fixed showing not valid data on page

--- src/components/usersFinder.tsx
+++ src/components/usersFinder.tsx
@@ -1,20 +1,28 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { SearchBar } from '@/components/searchBar'
 import { UsersList } from '@/components/usersList'
 import { IUser } from '@/app/types'
+import { useRouter } from 'next/navigation'
 
 interface Props {
   usersList: IUser[] | null
 }
 
 export const UsersFinder: React.FC<Props> = ({ usersList }) => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(true)
   const [inputText, setInputText] = useState('')
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     let lowerCase = e.target.value.toLowerCase()
     setInputText(lowerCase)
   }
+
+  useEffect(() => {
+    router.refresh()
+    setIsLoading(false)
+  }, [])
 
   return (
     <>
@@ -25,7 +33,10 @@ export const UsersFinder: React.FC<Props> = ({ usersList }) => {
         value={inputText}
         onChange={inputHandler}
       />
-      <UsersList users={usersList} input={inputText} />
+      {/* isLoading ? <Loader /> : <UsersList />*/}
+      {isLoading ? null : (
+        <UsersList users={usersList} input={inputText} />
+      )}{' '}
     </>
   )
 }
