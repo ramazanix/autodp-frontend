b94f813899a808e3368b5d8c2ff2e80140bfa54c
Ramazan <ramazanui2002@gmail.com>
2023-12-15 00:51:01 +0300

    Removed unused code

--- src/app/(main)/users/[username]/settings/page.tsx
+++ src/app/(main)/users/[username]/settings/page.tsx
@@ -1,7 +1,6 @@
 import { PrivateWarning } from '@/components/privateWarning'
 import { UserSettings } from '@/components/userSettings'
 import { usersService } from '@/services'
-import { jwtDecode } from 'jwt-decode'
 import { cookies } from 'next/headers'
 
 export default async function UserSettingsPage({
@@ -13,8 +12,6 @@ export default async function UserSettingsPage({
 
   if (accessToken) {
     const userInfo = await usersService.users.get(params.username, accessToken)
-    const parsedJWT = jwtDecode(accessToken)
-    const owner = params.username === parsedJWT.sub
 
     return <UserSettings userInfo={userInfo} accessToken={accessToken} />
   }
